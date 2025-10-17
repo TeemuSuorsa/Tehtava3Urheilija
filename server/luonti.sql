a275ea977c39928d6f405e88aa431a6c1faad625
TeemuSuorsa <2306064@edu.karelia.fi>
2025-10-17 10:34:58 +0300

    Lisätty vielä esimerkkitäyttö tietokantaan

--- server/luonti.sql
+++ server/luonti.sql
@@ -12,3 +12,6 @@ CREATE TABLE Urheilijat (
   laji VARCHAR(50),
   saavutukset TEXT
 );
+
+INSERT INTO Urheilijat (etunimi, sukunimi, kutsumanimi, syntymavuosi, paino, kuvaLinkki, laji, saavutukset) VALUES
+('Matti', 'Nykänen', 'Masa', '1963-07-17', 75.0, 'https://fi.wikipedia.org/wiki/Matti_Nyk%C3%A4nen#/media/Tiedosto:Matti_Nyk%C3%A4nen_2014-01-30_001.jpg', 'Mäkihyppy', '14 kultaa, 4 hopeaa, 8 pronssia'),
